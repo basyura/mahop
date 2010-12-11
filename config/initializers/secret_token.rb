bec5d07178a5ccb0c33fc695055cdfa424bdca6f
basyura <basyura@gmail.com>
2010-12-11 23:14:49 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mahop::Application.config.secret_token = '8448140d9c16b656911588be381179218c31f15a8ee463e50ac71289213afb06b887facc43f74d3ae135928e205aa27a509bf10a08553a134c4a3456664e82d6'
